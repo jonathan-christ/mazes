3a211f163a4304e5230f002e81fe8026e02b24cc
Matthew Cedric Calaycay <matthewc.calaycay@gmail.com>
2025-07-13 21:23:53 +0800

    feat: create Cell component for rendering maze cells with dynamic wall borders

---
+++ src/lib/components/Cell.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+    import type { Cell } from "../types";
+    import clsx from "clsx";
+
+    let { cell }: { cell: Cell } = $props();
+    const classes = $derived(clsx("h-4 w-4 shrink-0 border-black border-solid border-0", {
+        "border-t-4": cell.walls.top,
+        "border-r-4": cell.walls.right,
+        "border-b-4": cell.walls.bottom,
+        "border-l-4": cell.walls.left,
+    }));
+</script>
+
+<div class={classes}></div>
