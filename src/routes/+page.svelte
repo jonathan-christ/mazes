3a9e7fd7baf8ba3da4a6634390a6f6d9c08d2bbd
Matthew Cedric Calaycay <matthewc.calaycay@gmail.com>
2025-07-13 21:28:05 +0800

    feat: update main page to include Maze component and set layout for maze rendering

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,8 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script lang="ts">
+	import Maze from '$lib/components/Maze.svelte';
+	import { MAZE_SIZE } from '$lib/const';
+</script>
+
+<div class="h-screen w-screen">
+	<Maze size={MAZE_SIZE.large} />
+</div>
