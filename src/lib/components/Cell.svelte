ef3f168bffe026164e4ea708089f4b961838b576
Matthew Cedric Calaycay <matthewc.calaycay@gmail.com>
2025-07-14 03:04:59 +0800

    feat: enhance Cell component to support placement states with background color changes

--- src/lib/components/Cell.svelte
+++ src/lib/components/Cell.svelte
@@ -2,12 +2,14 @@
     import type { Cell } from "../types";
     import clsx from "clsx";
 
-    let { cell }: { cell: Cell } = $props();
+    let { cell, place }: { cell: Cell, place: "start" | "end" | "mid" } = $props();
     const classes = $derived(clsx("h-2 w-2 shrink-0 border-black border-solid border-0", {
         "border-t": cell.walls.top,
         "border-r": cell.walls.right,
         "border-b": cell.walls.bottom,
         "border-l": cell.walls.left,
+        "bg-green-600": place === "start",
+        "bg-red-600": place === "end",
     }));
 </script>
 
