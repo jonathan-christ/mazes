2ec5e0a9084b4a8e7060e8a85757cfdf939cbd91
Matthew Cedric Calaycay <matthewc.calaycay@gmail.com>
2025-07-14 05:25:44 +0800

    style: refactor Cell component for improved readability and add support for path highlighting

--- src/lib/components/Cell.svelte
+++ src/lib/components/Cell.svelte
@@ -1,16 +1,19 @@
 <script lang="ts">
-    import type { Cell } from "../types";
-    import clsx from "clsx";
+	import type { Cell } from '../types';
+	import clsx from 'clsx';
 
-    let { cell, place }: { cell: Cell, place: "start" | "end" | "mid" } = $props();
-    const classes = $derived(clsx("h-2 w-2 shrink-0 border-black border-solid border-0", {
-        "border-t": cell.walls.top,
-        "border-r": cell.walls.right,
-        "border-b": cell.walls.bottom,
-        "border-l": cell.walls.left,
-        "bg-green-600": place === "start",
-        "bg-red-600": place === "end",
-    }));
+	let { cell, place }: { cell: Cell; place: 'start' | 'end' | 'mid' } = $props();
+	const classes = $derived(
+		clsx('h-2 w-2 shrink-0 border-black border-solid border-0', {
+			'border-t': cell.walls.top,
+			'border-r': cell.walls.right,
+			'border-b': cell.walls.bottom,
+			'border-l': cell.walls.left,
+			'bg-green-600': place === 'start',
+			'bg-red-600': place === 'end',
+			'bg-yellow-400': cell.path,
+		})
+	);
 </script>
 
 <div class={classes}></div>
