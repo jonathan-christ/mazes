30915d89625cdf386d72f4e8178576aa29f95725
Matthew Cedric Calaycay <matthewc.calaycay@gmail.com>
2025-07-14 02:23:26 +0800

    style: adjust Cell component dimensions and border styles for improved visual representation

--- src/lib/components/Cell.svelte
+++ src/lib/components/Cell.svelte
@@ -3,11 +3,11 @@
     import clsx from "clsx";
 
     let { cell }: { cell: Cell } = $props();
-    const classes = $derived(clsx("h-4 w-4 shrink-0 border-black border-solid border-0", {
-        "border-t-4": cell.walls.top,
-        "border-r-4": cell.walls.right,
-        "border-b-4": cell.walls.bottom,
-        "border-l-4": cell.walls.left,
+    const classes = $derived(clsx("h-2 w-2 shrink-0 border-black border-solid border-0", {
+        "border-t": cell.walls.top,
+        "border-r": cell.walls.right,
+        "border-b": cell.walls.bottom,
+        "border-l": cell.walls.left,
     }));
 </script>
 
