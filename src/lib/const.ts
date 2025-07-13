73dedf5516b8ae7630c719deec3ed8b11b532a89
Matthew Cedric Calaycay <matthewc.calaycay@gmail.com>
2025-07-14 03:04:49 +0800

    fix: update large maze dimensions to correct width for improved gameplay experience

--- src/lib/const.ts
+++ src/lib/const.ts
@@ -7,7 +7,7 @@ export const MAZE_SIZE: {
 } = {
     small: { width: 10, height: 10 },
     medium: { width: 20, height: 20 },
-    large: { width: 30, height: 30 }
+    large: { width: 60, height: 20 }
 };
 
 export const TOP = { x: 0, y: -1 }
